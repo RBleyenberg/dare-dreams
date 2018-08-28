ab1aeceeb677929c27e1155be4643ccb833758ca
remco <remcobleijenberg@gmail.com>
2018-08-28 22:40:39 +0200

    first setup

--- frontend/src/app/state/app.reducer.ts
+++ frontend/src/app/state/app.reducer.ts
@@ -4,8 +4,11 @@ import { storeFreeze } from 'ngrx-store-freeze';
 import { environment } from '../../environments/environment';
 import { AppState } from './app.interfaces';
 
+import { reducer as articleReducer } from './articles/article.reducers';
+
 export const appReducer: ActionReducerMap<AppState> = {
   router: routerReducer,
+  article: articleReducer,
 };
 
 export const appMetaReducers: MetaReducer<AppState>[] = !environment.production
