9821b392c36470145590ed66685d96eeb195ea43
remco <remcobleijenberg@gmail.com>
2018-08-29 18:21:37 +0200

    best

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import {AnimationsService} from './layout/animation/animations.service';
+import {routeAnimations} from './layout/animation/route.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [routeAnimations]
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private animationService: AnimationsService) {}
+
+
 }
