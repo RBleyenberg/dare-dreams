1b6fe979d6adadce8c410c528d346b36d4732ae5
remco <remcobleijenberg@gmail.com>
2018-08-30 20:45:57 +0200

    Working test with jest

--- frontend/src/app/layout/layout.component.spec.ts
+++ frontend/src/app/layout/layout.component.spec.ts
@@ -1,25 +1,66 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LayoutModule } from '@angular/cdk/layout';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ConfigureFn, configureTests } from '../../test-config.helper';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { LayoutComponent } from './layout.component';
 
 describe('LayoutComponent', () => {
-  let component: LayoutComponent;
-  let fixture: ComponentFixture<LayoutComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LayoutComponent ]
+  let fixture: ComponentFixture<LayoutComponent>;
+  let component: LayoutComponent;
+
+  beforeEach(
+    async(() => {
+      const configure: ConfigureFn = testBed => {
+        testBed.configureTestingModule({
+          declarations: [LayoutComponent],
+          imports: [
+            NoopAnimationsModule,
+            RouterTestingModule,
+            BrowserAnimationsModule,
+            MatToolbarModule,
+            MatIconModule,
+            MatListModule,
+            LayoutModule,
+            MatSidenavModule
+          ],
+          schemas: [NO_ERRORS_SCHEMA],
+        });
+      };
+
+      configureTests(configure).then(testBed => {
+        fixture = testBed.createComponent(LayoutComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+      });
     })
-    .compileComponents();
+  );
+
+  it(
+    'should create the layout',
+    async(() => {
+      const app = component;
+      expect(app).toBeTruthy();
+    })
+  );
+
+  it('snaps', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('looks async but is synchronous', <any>fakeAsync((): void => {
+    let flag = false;
+    setTimeout(() => {
+      flag = true;
+    }, 100);
+    expect(flag).toBe(false);
+    tick(50);
+    expect(flag).toBe(false);
+    tick(50);
+    expect(flag).toBe(true);
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LayoutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
